64780c493d8f4538262559a3ebe34f5d6da2a55f
JohnPingu <jules.pigne@etu.univ-smb.fr>
2024-01-09 15:01:09 +0100

    Début code main window + ajout portee ruee

--- MaelKiller/MainWindow.xaml.cs
+++ MaelKiller/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MaelKiller
 {
@@ -20,9 +21,20 @@ namespace MaelKiller
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool gauche, droite, haut, bas = false;
+        private List<Rectangle> objetsSuppr = new List<Rectangle>();
+        private DispatcherTimer intervalle = new DispatcherTimer();
+
         public MainWindow()
         {
             InitializeComponent();
+            intervalle.Tick += MoteurJeu;
+            intervalle.Interval = TimeSpan.FromMilliseconds(16);
+        }
+        private void MoteurJeu(object sender, EventArgs e)
+        {
+            Rect joueur = new Rect(Canvas.GetLeft(rect_Joueur), Canvas.GetTop(rect_Joueur), rect_Joueur.Width, rect_Joueur.Height);
         }
     }
+   
 }
