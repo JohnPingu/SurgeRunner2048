7cdcac7d21ed56e0c1630bf5c734d863e89dfa22
JohnPingu <jules.pigne@etu.univ-smb.fr>
2024-01-09 15:11:02 +0100

    intégration menu

--- MaelKiller/MainWindow.xaml.cs
+++ MaelKiller/MainWindow.xaml.cs
@@ -21,15 +21,63 @@ namespace MaelKiller
     /// </summary>
     public partial class MainWindow : Window
     {
-        private bool gauche, droite, haut, bas = false;
+        private bool gauche, droite, haut, bas, ruee = false;
         private List<Rectangle> objetsSuppr = new List<Rectangle>();
         private DispatcherTimer intervalle = new DispatcherTimer();
 
         public MainWindow()
         {
             InitializeComponent();
+            Menu menu = new Menu();
+            menu.ShowDialog();
             intervalle.Tick += MoteurJeu;
             intervalle.Interval = TimeSpan.FromMilliseconds(16);
+        }
+        private void FenetrePrincipale_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                gauche = true;
+            }
+            if (e.Key == Key.Right)
+            {
+                droite = true;
+            }
+            if (e.Key == Key.Up)
+            {
+                haut = true;
+            }
+            if (e.Key == Key.Down)
+            {
+                bas = true;
+            }
+            if (e.Key == Key.Space)
+            {
+                ruee = true;
+            }
+        }
+        private void FenetrePrincipale_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                gauche = false;
+            }
+            if (e.Key == Key.Right)
+            {
+                droite = false;
+            }
+            if (e.Key == Key.Up)
+            {
+                haut = false;
+            }
+            if (e.Key == Key.Down)
+            {
+                bas = false;
+            }
+            if (e.Key == Key.Space)
+            {
+                ruee = false;
+            }
         }
         private void MoteurJeu(object sender, EventArgs e)
         {
