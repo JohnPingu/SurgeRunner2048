906678cb9b85003a30cdf7bc3721939ff70cd6bd
Tarek Bendaida <tarek.bendaida@etu.univ-smb.fr>
2024-01-09 10:26:31 +0100

    Correction classe monstre + Création de la classe Support

--- MaelKiller/Supports.cs
+++ MaelKiller/Supports.cs
@@ -8,5 +8,64 @@ namespace MaelKiller
 {
     internal class Supports
     {
+		private string nom;
+        private string multiplieur;
+        private string description;
+
+        public Supports(string nom, string multiplieur, string description)
+        {
+            Nom = nom;
+            Multiplieur = multiplieur;
+            description = description;
+        }
+
+        public string Nom
+		{
+			get { return nom; }
+			set 
+            {
+                if (String.IsNullOrEmpty(value)) throw new ArgumentException("Le champ nom ne doit pas être vide");
+                nom = value; 
+            }
+		}
+
+        public string Multiplieur
+        {
+            get { return multiplieur; }
+            set 
+            {
+                if (value != "vitesse" && value != "pv" && value != "regen" && value != "attraction") throw new ArgumentException("Le multiplieur doit être parmmis ces propositions: vitesse,pv,regen,attraction,");
+                multiplieur = value; 
+            }
+        }
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                description = value;
+            }
+        }
+
+        public void ApplicationDesStats(Supports support, Joueur joueur)
+        {
+        
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Supports supports &&
+                   Nom == supports.Nom;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Nom, Multiplieur);
+        }
+
+        public override string? ToString()
+        {
+            return "Nom : " + nom + "\nMultiplieur : " + multiplieur + "\nDescription : " + description;
+        }
     }
 }
